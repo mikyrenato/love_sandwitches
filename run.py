78840d858e54c17566138310d6f0a53cb7223b40
mikyrenato <mikyrenato@yahoo.com>
2022-08-17 08:29:50 +0000

    Commit

--- run.py
+++ run.py
@@ -118,6 +118,21 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+    # headings = SHEET.worksheet('stock').row_values(1)
+    print("Make the following numbers of sandwiches for next market:\n")
+    # new_data = {}
+    # for heading, stock_num in zip(headings, data):
+    #     new_data[heading] = stock_num
+    # return new_data
+    return {heading: data for heading, data in zip(headings, data)}
+stock_values = get_stock_values(stock_data)
+print(stock_values)
+
 
 def main():
     """
